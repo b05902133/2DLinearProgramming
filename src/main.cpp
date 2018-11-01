608cd5846549ea0b1b075bc2405b148d97e23463
flotisable <s09930698@gmail.com>
2018-11-01 15:30:51 +0800

    add algorithm of selection problem

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+#include "Select.h"
+
 int main()
 {
+  Select engine;
+
+  cout << engine.select( { 1, 4, 2, 3, 5, 7, 6, 8, 10, 9 }, 3 ) << "\n";
+
   return 0;
 }
