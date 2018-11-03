7dfa7302f37f16c7ffc71fcfd1e92b68811c485c
flotisable <s09930698@gmail.com>
2018-11-03 14:52:06 +0800

    add io

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <cmath>
+#include <limits>
 using namespace std;
 
-#include "Select.h"
+#include "LP.h"
 
 int main()
 {
-  Select engine;
+  LP2D                      solver;
+  vector<LP2D::Constraint>  constraints;
+  int                       constraintNum;
+  double                    yMin;
 
-  cout << engine.select( { 1, 4, 2, 3, 5, 7, 6, 8, 10, 9 }, 3 ) << "\n";
+  cin >> constraintNum;
+
+  constraints.reserve( constraintNum );
+
+  for( int i = 0 ; i < constraintNum ; ++i )
+  {
+     int a, b, c;
+
+     cin >> a >> b >> c;
+
+     constraints.push_back( make_tuple( a, b, c ) );
+  }
+
+  yMin = round( solver.solve( constraints ) );
+
+  if( yMin == numeric_limits<double>::max() ) // nosolution
+    cout << "NA\n";
+  else if( yMin == numeric_limits<double>::min() ) // -infinity
+    cout << "-INF\n";
+  else
+    cout << yMin << "\n";
 
   return 0;
 }
